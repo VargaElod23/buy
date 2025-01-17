2c84ec0845d6ed2ca4cfb66f4cf6fea987bf4403
VargaElod23 <vargaelod23@gmail.com>
2025-01-17 19:08:06 +0100

    implement PoC

---
+++ src/app/Buy.tsx
@@ -0,0 +1,41 @@
+import { createThirdwebClient } from "thirdweb";
+import { ThirdwebProvider, ConnectButton, PayEmbed } from "thirdweb/react";
+import { BackgroundLines } from "./components/BgLines";
+
+const clientId = process.env.THIRDWEB_CLIENT_ID;
+
+if (!clientId) {
+  throw new Error("THIRDWEB_CLIENT_ID  must be set");
+}
+
+const client = createThirdwebClient({
+  clientId,
+});
+
+export default function BuyPage() {
+  return (
+    <ThirdwebProvider>
+      <BackgroundLines>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            position: "relative",
+            zIndex: 1,
+          }}
+        >
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl border-b">
+            Buy Taraxa
+          </h1>
+          <PayEmbed client={client} />
+          <div style={{ position: "absolute", top: 5, right: 5 }}>
+            <ConnectButton client={client} />
+          </div>
+        </div>
+      </BackgroundLines>
+    </ThirdwebProvider>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import BuyPage from "./Buy";
+
+export default function Home() {
+  return (
+    <>
+      <BuyPage />
+    </>
+  );
+}
